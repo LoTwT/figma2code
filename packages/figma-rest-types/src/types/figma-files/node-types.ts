c1854f50b8b89106d8c97c34c19b4a40e2f353d7
LoTwT <709937065@qq.com>
2023-07-26 13:24:31 +0800

    feat(figma-rest-types): some node types

--- packages/figma-rest-types/src/types/figma-files/node-types.ts
+++ packages/figma-rest-types/src/types/figma-files/node-types.ts
@@ -10,14 +10,22 @@ import type {
   LayoutConstraint,
   LayoutGrid,
   Paint,
+  PaintOverride,
   PrototypeDevice,
   Rectangle,
+  StrokeWeights,
   StyleType,
   Transform,
   Vector,
 } from ".."
 
-export type Node = DocumentNode | CanvasNode | FrameNode
+export type Node =
+  | DocumentNode
+  | CanvasNode
+  | FrameNode
+  | GroupNode
+  | SectionNode
+  | VectorNode
 
 /**
  * @see https://www.figma.com/developers/api#document-props
@@ -355,6 +363,224 @@ export interface FrameNode extends BaseNode {
    */
   styles: Record<StyleType, string>
 }
+
+export type GroupNode = Omit<FrameNode, "type"> & {
+  readonly type: "GROUP"
+}
+
+export interface SectionNode extends BaseNode {
+  readonly type: "SECTION"
+  /**
+   * Whether the contents of the section are visible
+   * @default false
+   */
+  sectionContentsHidden: boolean
+  /**
+   * Whether the section is marked Ready for dev.
+   * If the section is marked Ready for dev, this property contains an object.
+   * "devStatus": {
+   *    "type": "READY_FOR_DEV"
+   *  }
+   * If the section is not ready for development or otherwise unmarked, the property is null.
+   * @default null
+   */
+  devStatus: Nullable<Record<string, any>>
+  /**
+   * An array of fill paints applied to the node
+   * @default []
+   */
+  fills: Paint[]
+  /**
+   * An array of stroke paints applied to the node
+   * @default []
+   */
+  strokes: Paint[]
+  /**
+   * The weight of strokes on the node
+   */
+  strokeWeight: number
+  /**
+   * Position of stroke relative to vector outline, as a string enum
+   */
+  strokeAlign: StrokeAlign
+  /**
+   * Bounding box of the node in absolute space coordinates
+   */
+  absoluteBoundingBox: Rectangle
+  /**
+   * The bounds of the rendered node in the file in absolute space coordinates
+   */
+  absoluteRenderBounds: Rectangle
+}
+
+export interface VectorNode extends BaseNode {
+  readonly type: "VECTOR"
+  /**
+   * If true, layer is locked and cannot be edited
+   * @default false
+   */
+  locked: boolean
+  /**
+   * An array of export settings representing images to export from the node
+   * @default []
+   */
+  exportSettings: ExportSetting[]
+  /**
+   * How this node blends with nodes behind it in the scene (see blend mode section for more details)
+   */
+  blendMode: BlendMode
+  /**
+   * Keep height and width constrained to same ratio
+   * @default false
+   */
+  preserveRatio: boolean
+  layoutAlign: LayoutAlign
+  /**
+   * This property is applicable only for direct children of auto-layout frames, ignored otherwise.
+   * Determines whether a layer should stretch along the parent’s primary axis.
+   * A 0 corresponds to a fixed size and 1 corresponds to stretch
+   * @default 0
+   */
+  layoutGrow: number
+  /**
+   * Horizontal and vertical layout constraints for node
+   */
+  constraints: LayoutConstraint
+  /**
+   * Node ID of node to transition to in prototyping
+   * @default null
+   */
+  transitionNodeID: Nullable<string>
+  /**
+   * The duration of the prototyping transition on this node (in milliseconds)
+   * @default null
+   */
+  transitionDuration: Nullable<number>
+  /**
+   * The easing curve used in the prototyping transition on this node
+   * @default null
+   */
+  transitionEasing: Nullable<EasingType>
+  /**
+   * Opacity of the node
+   * @default 1
+   */
+  opacity: number
+  /**
+   * Bounding box of the node in absolute space coordinates
+   */
+  absoluteBoundingBox: Rectangle
+  /**
+   * The bounds of the rendered node in the file in absolute space coordinates
+   */
+  absoluteRenderBounds: Rectangle
+  /**
+   * An array of effects attached to this node (see effects section for more details)
+   * @default []
+   */
+  effects: Effect[]
+  /**
+   * Width and height of element.
+   * This is different from the width and height of the bounding box in that the absolute bounding box represents the element after scaling and rotation.
+   * Only present if geometry=paths is passed
+   */
+  size: Vector
+  /**
+   * The top two rows of a matrix that represents the 2D transform of this node relative to its parent.
+   * The bottom row of the matrix is implicitly always (0, 0, 1).
+   * Use to transform coordinates in geometry.
+   * Only present if geometry=paths is passed
+   */
+  relativeTransform: Transform
+  /**
+   * Does this node mask sibling nodes in front of it?
+   * @default false
+   */
+  isMask: boolean
+  /**
+   * An array of fill paints applied to the node
+   * @default []
+   */
+  fills: Paint[]
+  /**
+   * Only specified if parameter geometry=paths is used.
+   * An array of paths representing the object fill
+   */
+  fillGeometry: Path[]
+  /**
+   * Map from ID to PaintOverride for looking up fill overrides.
+   * To see which regions are overriden, you must use the `geometry=paths` option.
+   * Each path returned may have an `overrideId` which maps to this table.
+   */
+  fillOverrideTable: Record<number, PaintOverride>
+  /**
+   * An array of stroke paints applied to the node
+   * @default []
+   */
+  strokes: Paint[]
+  /**
+   * The weight of strokes on the node
+   */
+  strokeWeight: number
+  /**
+   * An object including the top, bottom, left, and right stroke weights.
+   * Only returned if individual stroke weights are used.
+   */
+  individualStrokeWeights: StrokeWeights
+  /**
+   * describing the end caps of vector paths.
+   * @default StrokeCap.NONE
+   */
+  strokeCap: StrokeCap
+  /**
+   * describing how corners in vector paths are rendered.
+   * @default StrokeJoin.MITER
+   */
+  strokeJoin: StrokeJoin
+  /**
+   * An array of floating point numbers describing the pattern of dash length and gap lengths that the vector path follows.
+   * For example a value of [1, 2] indicates that the path has a dash of length 1 followed by a gap of length 2, repeated.
+   * @default []
+   */
+  strokeDashes: number[]
+  /**
+   * Only valid if strokeJoin is "MITER".
+   * The corner angle, in degrees, below which strokeJoin will be set to "BEVEL" to avoid super sharp corners.
+   * By default this is 28.96 degrees.
+   * @default 28.96
+   */
+  strokeMiterAngle: number
+  /**
+   * Only specified if parameter geometry=paths is used.
+   * An array of paths representing the object stroke
+   */
+  strokeGeometry: Path[]
+  /**
+   * Position of stroke relative to vector outline, as a string enum
+   */
+  strokeAlign: StrokeAlign
+  /**
+   * A mapping of a StyleType to style ID (see Style) of styles present on this node.
+   * The style ID can be used to look up more information about the style in the top-level styles field.
+   */
+  styles: Record<StyleType, string>
+}
+
+export enum StrokeJoin {
+  MITER = "MITER",
+  BEVEL = "BEVEL",
+  ROUND = "ROUND",
+}
+
+export enum StrokeCap {
+  NONE = "NONE",
+  ROUND = "ROUND",
+  SQUARE = "SQUARE",
+  LINE_ARROW = "LINE_ARROW",
+  TRIANGLE_ARROW = "TRIANGLE_ARROW",
+}
+
+export type Path = any
 
 export enum StrokeAlign {
   /**
